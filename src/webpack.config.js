ac038bf4caef5230df183f976110897b90e0c210
Nicolas Orozco <nicolas.orozco@utp.edu.co>
2020-06-17 13:21:11 -0500

    cosas

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,5 +2,9 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 module.exports = {
   plugins: [
     new MonacoWebpackPlugin()
-  ]
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    disableHostCheck: true,
+  }
 };
